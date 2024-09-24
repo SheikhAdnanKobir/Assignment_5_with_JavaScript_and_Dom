0be92653c96503b071f028e90434c261a4827264
SheikhAdnanKobir <sheikhadnankabir500@gmail.com>
2024-09-24 21:06:49 +0600

    Whole website complete

--- js/liveWork.js
+++ js/liveWork.js
@@ -5,3 +5,150 @@ document.getElementById("donate").addEventListener("click",function(){
 document.getElementById("history").addEventListener("click",function(){
     btncolorChange("history");
 })
+
+
+document.getElementById("history").addEventListener("click",function(){
+    donathistory("history2");
+})
+document.getElementById("donate").addEventListener("click",function(){
+    donathistory("containt");
+})
+
+document.getElementById("as-sunnah3").addEventListener("click",function(){
+    const getvalue=convert("as-sunnah2");
+    const donate=convert2("my-balanc");
+    const prvusbal=convert2("as-sunnah1")
+    const text=convert3("as-sunnah-text")
+    const timestamp = new Date().toLocaleString();
+   
+
+    if(getvalue<=donate && getvalue>0){
+        const odd=donate-getvalue;
+        const even=getvalue+prvusbal;
+        document.getElementById("my-balanc").innerText=odd;
+        document.getElementById("as-sunnah1").innerText=even;
+
+        const p=document.createElement("p");
+        p.innerText=` ${getvalue} Taka is Donate for ${text}\n Date: ${timestamp}\n\n`;
+        document.getElementById("history2").appendChild(p);  
+
+        const modal=document.getElementById("my_modal_1");
+        modal.show();
+    }
+
+    else if(getvalue>donate){
+        alert("Insufficient balance")
+    }
+
+    else if(getvalue<=0 || (isNaN && getvalue!=="number")){
+        alert("Please enter a valid input")
+    }
+    else{
+        alert("Something is wrong")
+    }
+})
+
+
+document.getElementById("noakhali3").addEventListener("click",function(){
+    const getvalue=convert("noakhali2");
+    const donate=convert2("my-balanc");
+    const prvusbal=convert2("noakhali1")
+    const text=convert3("nookhali-text")
+    const timestamp = new Date().toLocaleString();
+   
+
+    if(getvalue<=donate && getvalue>0){
+        const odd=donate-getvalue;
+        const even=getvalue+prvusbal;
+        document.getElementById("my-balanc").innerText=odd;
+        document.getElementById("noakhali1").innerText=even;
+
+        const p=document.createElement("p");
+        p.innerText=` ${getvalue} Taka is ${text}\n Date: ${timestamp}\n\n`;
+        document.getElementById("history2").appendChild(p);  
+
+        const modal=document.getElementById("my_modal_1");
+        modal.show();
+    }
+
+    else if(getvalue>donate){
+        alert("Insufficient balance")
+    }
+
+    else if(getvalue<=0 || (isNaN && getvalue!=="number")){
+        alert("Please enter a valid input")
+    }
+    else{
+        alert("Something is wrong")
+    }
+})
+
+
+document.getElementById("feni3").addEventListener("click",function(){
+    const getvalue=convert("feni2");
+    const donate=convert2("my-balanc");
+    const prvusbal=convert2("feni1")
+    const text=convert3("feni-text")
+    const timestamp = new Date().toLocaleString();
+   
+
+    if(getvalue<=donate && getvalue>0){
+        const odd=donate-getvalue;
+        const even=getvalue+prvusbal;
+        document.getElementById("my-balanc").innerText=odd;
+        document.getElementById("feni1").innerText=even;
+
+        const p=document.createElement("p");
+        p.innerText=` ${getvalue} Taka is ${text}\n Date: ${timestamp}\n\n`;
+        document.getElementById("history2").appendChild(p);  
+
+        const modal=document.getElementById("my_modal_1");
+        modal.show();
+    }
+
+    else if(getvalue>donate){
+        alert("Insufficient balance")
+    }
+
+    else if(getvalue<=0 || (isNaN && getvalue!=="number")){
+        alert("Please enter a valid input")
+    }
+    else{
+        alert("Something is wrong")
+    }
+})
+
+
+document.getElementById("quota3").addEventListener("click",function(){
+    const getvalue=convert("quota2");
+    const donate=convert2("my-balanc");
+    const prvusbal=convert2("quota1")
+    const text=convert3("quota-text")
+    const timestamp = new Date().toLocaleString();
+   
+
+    if(getvalue<=donate && getvalue>0){
+        const odd=donate-getvalue;
+        const even=getvalue+prvusbal;
+        document.getElementById("my-balanc").innerText=odd;
+        document.getElementById("quota1").innerText=even;
+
+        const p=document.createElement("p");
+        p.innerText=` ${getvalue} Taka is ${text}\n\n Date: ${timestamp}\n\n`;
+        document.getElementById("history2").appendChild(p);  
+
+        const modal=document.getElementById("my_modal_1");
+        modal.show();
+    }
+
+    else if(getvalue>donate){
+        alert("Insufficient balance")
+    }
+
+    else if(getvalue<=0 || (isNaN && getvalue!=="number")){
+        alert("Please enter a valid input")
+    }
+    else{
+        alert("Something is wrong")
+    }
+})
--- js/processing.js
+++ js/processing.js
@@ -1,6 +1,29 @@
 function btncolorChange(id){
     document.getElementById("donate").classList.remove("btnColorPik");
     document.getElementById("history").classList.remove("btnColorPik");
-
+ 
     document.getElementById(id).classList.add("btnColorPik")
+}
+
+function donathistory(id){
+    document.getElementById("history2").classList.add("hidden");
+    document.getElementById("containt").classList.add("hidden");
+
+    document.getElementById(id).classList.remove("hidden")
+}
+
+
+function convert(id){
+    const getvalue= document.getElementById(id).value;
+    const getfloat=parseFloat(getvalue);
+    return getfloat;
+}
+function convert2(id){
+    const getvalue= document.getElementById(id).innerText;
+    const getfloat=parseFloat(getvalue);
+    return getfloat;
+}
+function convert3(id){
+    const getvalue= document.getElementById(id).innerText;
+    return getvalue;
 }
